a666233976242878f5eb8b0f8770dd42b49bd3fd
MarekAG <marekwstrzala@gmail.com>
2017-03-27 17:33:51 +0200

    added postgres scripts

---
+++ geo/insert_postgres.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+
+ogr2ogr -f "PostgreSQL" PG:"host=localhost dbname=postgres user=postgres password=postgres" "pedestriancrossing.geojson" -overwrite
---
+++ geo/script_postgres.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ogr2ogr -f "GeoJSON" result_postgres.geojson PG:"host=localhost dbname=postgres user=postgres password=postgres" -sql 'SELECT *
+FROM ogrgeojson
+WHERE ST_Distance_Sphere(wkb_geometry, ST_MakePoint(-5.935708910555356,54.6012368489165)) <= 10000;'
